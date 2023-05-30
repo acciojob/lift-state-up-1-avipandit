6c97d33f0546c6c20b0d4d7ab978df4253899174
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-05-30 12:11:07 +0530

    Create Modal.js

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+    <div>
+      <h3>Modal Content</h3>
+      <p>This is the modal content.</p>
+    </div>
+  );
+}
+
+export default Modal;
